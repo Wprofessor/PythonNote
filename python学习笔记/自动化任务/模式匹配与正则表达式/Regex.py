fc5a59987782800fbdb8fd2614f978fbd0e58d80
Wprofessor <wprofessor@gmail.com>
2018-11-09 19:51:35 +0800

    Wprofessor commit.

---
+++ python学习笔记/自动化任务/模式匹配与正则表达式/Regex.py
@@ -0,0 +1,13 @@
+import re
+
+begain = re.compile(r'^Hello')  # 匹配以‘Hello开始的字符串’
+print(begain.search('Hello world'))
+print(begain.search('He said hello.'))  # 输出None
+
+end = re.compile(r'\d$')  # 匹配以数字结尾的字符串
+print(end.search('fhdbs 5454'))
+print(end.search('sdgs dour'))  # 输出None
+
+whoel = re.compile(r'^\d+$')     #匹配以数字开头数字结尾的字符串（自己也必须如此）
+print(whoel.search('564645'))
+print(whoel.search('45645 45654'))      #输出None
